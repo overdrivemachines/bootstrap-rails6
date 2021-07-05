b7c8e1ea6693a5055e116afccc75dd760c607792
Dipen Chauhan <get.dipen@gmail.com>
2021-07-05 14:29:08 -0700

    Imported bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require.context("../images", true)
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+import "bootstrap"
+import "@popperjs/core"
+import "../src/my_script.js"
 
 Rails.start()
 Turbolinks.start()
